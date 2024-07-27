9840675b04b3782d6427f4646b661c55c3e3d63a
John Bomann <johnabomann@gmail.com>
2024-07-27 17:26:37 -0500

    Removed migration.js

--- module/blades.js
+++ module/blades.js
@@ -18,24 +18,32 @@ import { BladesCrewSheet } from "./blades-crew-sheet.js";
 import { BladesClockSheet } from "./blades-clock-sheet.js";
 import { BladesNPCSheet } from "./blades-npc-sheet.js";
 import { BladesFactionSheet } from "./blades-faction-sheet.js";
-import * as migrations from "./migration.js";
 
 window.BladesHelpers = BladesHelpers;
 
 /* -------------------------------------------- */
 /*  Foundry VTT Initialization                  */
 /* -------------------------------------------- */
-Hooks.once("init", async function() {
+Hooks.once("init", async function () {
   console.log(`Initializing Blades In the Dark System`);
 
   game.blades = {
-    dice: bladesRoll
+    dice: bladesRoll,
   };
   game.system.bladesClocks = {
-    sizes: [ 4, 6, 8, 10, 12 ]
+    sizes: [4, 6, 8, 10, 12],
   };
 
-  game.system.traumas = [ "cold", "haunted", "obsessed", "paranoid", "reckless", "soft", "unstable", "vicious" ];
+  game.system.traumas = [
+    "cold",
+    "haunted",
+    "obsessed",
+    "paranoid",
+    "reckless",
+    "soft",
+    "unstable",
+    "vicious",
+  ];
 
   CONFIG.Item.documentClass = BladesItem;
   CONFIG.Actor.documentClass = BladesActor;
@@ -49,17 +57,19 @@ Hooks.once("init", async function() {
   Actors.registerSheet("blades", BladesActorSheet, { types: ["character"], makeDefault: true });
   Actors.registerSheet("blades", BladesCrewSheet, { types: ["crew"], makeDefault: true });
   Actors.registerSheet("blades", BladesFactionSheet, { types: ["factions"], makeDefault: true });
-  Actors.registerSheet("blades", BladesClockSheet, { types: ["\uD83D\uDD5B clock"], makeDefault: true });
+  Actors.registerSheet("blades", BladesClockSheet, {
+    types: ["\uD83D\uDD5B clock"],
+    makeDefault: true,
+  });
   Actors.registerSheet("blades", BladesNPCSheet, { types: ["npc"], makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("blades", BladesItemSheet, {makeDefault: true});
+  Items.registerSheet("blades", BladesItemSheet, { makeDefault: true });
   await preloadHandlebarsTemplates();
 
-  Actors.registeredSheets.forEach(element => console.log(element.Actor.name));
-
+  Actors.registeredSheets.forEach((element) => console.log(element.Actor.name));
 
   // Is the value Turf side.
-  Handlebars.registerHelper('is_turf_side', function(value, options) {
+  Handlebars.registerHelper("is_turf_side", function (value, options) {
     if (["left", "right", "top", "bottom"].includes(value)) {
       return options.fn(this);
     } else {
@@ -68,25 +78,24 @@ Hooks.once("init", async function() {
   });
 
   // Multiboxes.
-  Handlebars.registerHelper('multiboxes', function(selected, options) {
-
+  Handlebars.registerHelper("multiboxes", function (selected, options) {
     let html = options.fn(this);
 
     // Fix for single non-array values.
-    if ( !Array.isArray(selected) ) {
+    if (!Array.isArray(selected)) {
       selected = [selected];
     }
 
-    if (typeof selected !== 'undefined') {
-      selected.forEach(selected_value => {
+    if (typeof selected !== "undefined") {
+      selected.forEach((selected_value) => {
         if (selected_value !== false) {
           let escapedValue = RegExp.escape(Handlebars.escapeExpression(selected_value));
-          let rgx = new RegExp(' value=\"' + escapedValue + '\"');
+          let rgx = new RegExp(' value="' + escapedValue + '"');
           let oldHtml = html;
           html = html.replace(rgx, "$& checked");
-          while( ( oldHtml === html ) && ( escapedValue >= 0 ) ){
+          while (oldHtml === html && escapedValue >= 0) {
             escapedValue--;
-            rgx = new RegExp(' value=\"' + escapedValue + '\"');
+            rgx = new RegExp(' value="' + escapedValue + '"');
             html = html.replace(rgx, "$& checked");
           }
         }
@@ -96,8 +105,7 @@ Hooks.once("init", async function() {
   });
 
   // Trauma Counter
-  Handlebars.registerHelper('traumacounter', function(selected, options) {
-
+  Handlebars.registerHelper("traumacounter", function (selected, options) {
     let html = options.fn(this);
 
     var count = 0;
@@ -109,32 +117,31 @@ Hooks.once("init", async function() {
 
     //if (count > 4) count = 4;
 
-    const rgx = new RegExp(' value=\"' + count + '\"');
+    const rgx = new RegExp(' value="' + count + '"');
     return html.replace(rgx, "$& checked");
-
   });
 
   // NotEquals handlebar.
-  Handlebars.registerHelper('noteq', (a, b, options) => {
-    return (a !== b) ? options.fn(this) : '';
+  Handlebars.registerHelper("noteq", (a, b, options) => {
+    return a !== b ? options.fn(this) : "";
   });
 
   //Less than comparison
-  Handlebars.registerHelper('lteq', (a, b) => {
-    return (a <= b);
+  Handlebars.registerHelper("lteq", (a, b) => {
+    return a <= b;
   });
 
   //Greater than comparison
-  Handlebars.registerHelper('gteq', (a, b) => {
-    return (a >= b);
+  Handlebars.registerHelper("gteq", (a, b) => {
+    return a >= b;
   });
 
-  Handlebars.registerHelper('oneless', (a) => {
-    return (a - 1);
+  Handlebars.registerHelper("oneless", (a) => {
+    return a - 1;
   });
 
   // ReputationTurf handlebar.
-  Handlebars.registerHelper('repturf', (turfs_amount, options) => {
+  Handlebars.registerHelper("repturf", (turfs_amount, options) => {
     let html = options.fn(this);
     var turfs_amount_int = parseInt(turfs_amount);
 
@@ -144,28 +151,31 @@ Hooks.once("init", async function() {
     }
 
     for (let i = 13 - turfs_amount_int; i <= 12; i++) {
-      const rgx = new RegExp(' value=\"' + i + '\"');
+      const rgx = new RegExp(' value="' + i + '"');
       html = html.replace(rgx, "$& disabled");
     }
     return html;
   });
 
-  Handlebars.registerHelper('crew_vault_coins', (max_coins, options) => {
-
+  Handlebars.registerHelper("crew_vault_coins", (max_coins, options) => {
     let html = options.fn(this);
     for (let i = 1; i <= max_coins; i++) {
-
-      html += "<input type=\"radio\" id=\"crew-coins-vault-" + i + "\" data-dType=\"Number\" name=\"system.vault.value\" value=\"" + i + "\"><label for=\"crew-coins-vault-" + i + "\"></label>";
+      html +=
+        '<input type="radio" id="crew-coins-vault-' +
+        i +
+        '" data-dType="Number" name="system.vault.value" value="' +
+        i +
+        '"><label for="crew-coins-vault-' +
+        i +
+        '"></label>';
     }
 
     return html;
   });
 
-  Handlebars.registerHelper('crew_experience', (_id, options) => {
-
+  Handlebars.registerHelper("crew_experience", (_id, options) => {
     let html = options.fn(this);
     for (let i = 1; i <= 10; i++) {
-
       html += `<input type="radio" id="crew-${_id}-experience-${i}" data-dType="Number" name="system.experience" value="${i}" dtype="Radio"><label for="crew-${_id}-experience-${i}"></label>`;
     }
 
@@ -173,17 +183,15 @@ Hooks.once("init", async function() {
   });
 
   // Enrich the HTML replace /n with <br>
-  Handlebars.registerHelper('html', (options) => {
-
-    let text = options.hash['text'].replace(/\n/g, "<br />");
+  Handlebars.registerHelper("html", (options) => {
+    let text = options.hash["text"].replace(/\n/g, "<br />");
 
     return new Handlebars.SafeString(text);
   });
 
   // times_from_1 left as legacy code to not break Alternate Sheets compatibility
-  Handlebars.registerHelper('times_from_1', function(n, block) {
-
-    var accum = '';
+  Handlebars.registerHelper("times_from_1", function (n, block) {
+    var accum = "";
     for (var i = 1; i <= n; ++i) {
       accum += block.fn(i);
     }
@@ -191,9 +199,8 @@ Hooks.once("init", async function() {
   });
 
   // times_from_0 left as legacy code to not break Alternate Sheets compatibility
-  Handlebars.registerHelper('times_from_0', function(n, block) {
-
-    var accum = '';
+  Handlebars.registerHelper("times_from_0", function (n, block) {
+    var accum = "";
     for (var i = 0; i <= n; ++i) {
       accum += block.fn(i);
     }
@@ -207,9 +214,8 @@ Hooks.once("init", async function() {
   // {{#times_from 1 10}}
   //   <span>{{this}}</span>
   // {{/times_from}}
-  Handlebars.registerHelper('times_from', function(start, n, block) {
-
-    let accum = '';
+  Handlebars.registerHelper("times_from", function (start, n, block) {
+    let accum = "";
     for (let i = start; i <= n; ++i) {
       accum += block.fn(i);
     }
@@ -219,134 +225,118 @@ Hooks.once("init", async function() {
   // Concat helper
   // https://gist.github.com/adg29/f312d6fab93652944a8a1026142491b1
   // Usage: (concat 'first 'second')
-  Handlebars.registerHelper('concat', function() {
-    var outStr = '';
-    for(var arg in arguments){
-        if(typeof arguments[arg]!='object'){
-            outStr += arguments[arg];
-        }
+  Handlebars.registerHelper("concat", function () {
+    var outStr = "";
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != "object") {
+        outStr += arguments[arg];
+      }
     }
     return outStr;
   });
-
 
   /**
    * @inheritDoc
    * Takes label from Selected option instead of just plain value.
    */
 
-  Handlebars.registerHelper('selectOptionsWithLabel', function(choices, options) {
-
-    const localize = options.hash['localize'] ?? false;
-    let selected = options.hash['selected'] ?? null;
-    let blank = options.hash['blank'] || null;
+  Handlebars.registerHelper("selectOptionsWithLabel", function (choices, options) {
+    const localize = options.hash["localize"] ?? false;
+    let selected = options.hash["selected"] ?? null;
+    let blank = options.hash["blank"] || null;
     selected = selected instanceof Array ? selected.map(String) : [String(selected)];
 
     // Create an option
     const option = (key, object) => {
-      if ( localize ) object.label = game.i18n.localize(object.label);
+      if (localize) object.label = game.i18n.localize(object.label);
       let isSelected = selected.includes(key);
-      html += `<option value="${key}" ${isSelected ? "selected" : ""}>${object.label}</option>`
+      html += `<option value="${key}" ${isSelected ? "selected" : ""}>${object.label}</option>`;
     };
 
     // Create the options
     let html = "";
-    if ( blank ) option("", blank);
-    Object.entries(choices).forEach(e => option(...e));
+    if (blank) option("", blank);
+    Object.entries(choices).forEach((e) => option(...e));
 
     return new Handlebars.SafeString(html);
   });
-
 
   /**
    * Create appropriate Blades clock
    */
   // Clocks in color for Clock Actors
-  Handlebars.registerHelper('blades-clock-color', function(parameter_name, type, color, current_value, uniq_id) {
+  Handlebars.registerHelper(
+    "blades-clock-color",
+    function (parameter_name, type, color, current_value, uniq_id) {
+      let html = "";
 
-    let html = '';
+      if (current_value === null || current_value === "null") {
+        current_value = 0;
+      }
+      if (color === undefined) {
+        color = "black";
+      }
 
-    if (current_value === null || current_value === 'null') {
-      current_value = 0;
-    }
-	if (color === undefined) {
-      color = "black";
-    }
+      if (parseInt(current_value) > parseInt(type)) {
+        current_value = type;
+      }
 
-    if (parseInt(current_value) > parseInt(type)) {
-      current_value = type;
-    }
+      // Label for 0
+      html += `<label class="clock-zero-label" for="clock-0-${uniq_id}}"><i class="fab fa-creative-commons-zero nullifier"></i></label>`;
+      html += `<div id="blades-clock-${uniq_id}" class="blades-clock clock-${type} clock-${type}-${current_value}" style="background-image:url('systems/blades-in-the-dark/themes/${color}/${type}clock_${current_value}.svg');">`;
 
-    // Label for 0
-    html += `<label class="clock-zero-label" for="clock-0-${uniq_id}}"><i class="fab fa-creative-commons-zero nullifier"></i></label>`;
-    html += `<div id="blades-clock-${uniq_id}" class="blades-clock clock-${type} clock-${type}-${current_value}" style="background-image:url('systems/blades-in-the-dark/themes/${color}/${type}clock_${current_value}.svg');">`;
+      let zero_checked = parseInt(current_value) === 0 ? "checked" : "";
+      html += `<input type="radio" value="0" id="clock-0-${uniq_id}}" data-dType="String" name="${parameter_name}" ${zero_checked}>`;
 
-    let zero_checked = (parseInt(current_value) === 0) ? 'checked' : '';
-    html += `<input type="radio" value="0" id="clock-0-${uniq_id}}" data-dType="String" name="${parameter_name}" ${zero_checked}>`;
-
-    for (let i = 1; i <= parseInt(type); i++) {
-      let checked = (parseInt(current_value) === i) ? 'checked' : '';
-      html += `
+      for (let i = 1; i <= parseInt(type); i++) {
+        let checked = parseInt(current_value) === i ? "checked" : "";
+        html += `
         <input type="radio" value="${i}" id="clock-${i}-${uniq_id}" data-dType="String" name="${parameter_name}" ${checked}>
         <label for="clock-${i}-${uniq_id}"></label>
       `;
-    }
+      }
 
-    html += `</div>`;
-    return html;
-  });
+      html += `</div>`;
+      return html;
+    }
+  );
   // Clocks in black for clocks embedded in sheets
-  Handlebars.registerHelper('blades-clock', function(parameter_name, type, current_value, uniq_id) {
+  Handlebars.registerHelper(
+    "blades-clock",
+    function (parameter_name, type, current_value, uniq_id) {
+      let html = "";
 
-    let html = '';
+      if (current_value === null || current_value === "null") {
+        current_value = 0;
+      }
 
-    if (current_value === null || current_value === 'null') {
-      current_value = 0;
-    }
+      if (parseInt(current_value) > parseInt(type)) {
+        current_value = type;
+      }
 
-    if (parseInt(current_value) > parseInt(type)) {
-      current_value = type;
-    }
+      // Label for 0
+      html += `<label class="clock-zero-label" for="clock-0-${uniq_id}}"><i class="fab fa-creative-commons-zero nullifier"></i></label>`;
+      html += `<div id="blades-clock-${uniq_id}" class="blades-clock clock-${type} clock-${type}-${current_value}" style="background-image:url('systems/blades-in-the-dark/themes/black/${type}clock_${current_value}.svg');">`;
 
-    // Label for 0
-    html += `<label class="clock-zero-label" for="clock-0-${uniq_id}}"><i class="fab fa-creative-commons-zero nullifier"></i></label>`;
-    html += `<div id="blades-clock-${uniq_id}" class="blades-clock clock-${type} clock-${type}-${current_value}" style="background-image:url('systems/blades-in-the-dark/themes/black/${type}clock_${current_value}.svg');">`;
+      let zero_checked = parseInt(current_value) === 0 ? "checked" : "";
+      html += `<input type="radio" value="0" id="clock-0-${uniq_id}}" data-dType="String" name="${parameter_name}" ${zero_checked}>`;
 
-    let zero_checked = (parseInt(current_value) === 0) ? 'checked' : '';
-    html += `<input type="radio" value="0" id="clock-0-${uniq_id}}" data-dType="String" name="${parameter_name}" ${zero_checked}>`;
-
-    for (let i = 1; i <= parseInt(type); i++) {
-      let checked = (parseInt(current_value) === i) ? 'checked' : '';
-      html += `
+      for (let i = 1; i <= parseInt(type); i++) {
+        let checked = parseInt(current_value) === i ? "checked" : "";
+        html += `
         <input type="radio" value="${i}" id="clock-${i}-${uniq_id}" data-dType="String" name="${parameter_name}" ${checked}>
         <label for="clock-${i}-${uniq_id}"></label>
       `;
+      }
+
+      html += `</div>`;
+      return html;
     }
+  );
 
-    html += `</div>`;
-    return html;
+  Handlebars.registerHelper("pc", function (string) {
+    return BladesHelpers.getProperCase(string);
   });
-  
-  Handlebars.registerHelper('pc', function( string ) {
-    return BladesHelpers.getProperCase( string );
-  });
-});
-
-/**
- * Once the entire VTT framework is initialized, check to see if we should perform a data migration
- */
-Hooks.once("ready", function() {
-
-  // Determine whether a system migration is required
-  const currentVersion = game.settings.get("bitd", "systemMigrationVersion");
-  const NEEDS_MIGRATION_VERSION = 2.15;
-
-  let needMigration = (currentVersion < NEEDS_MIGRATION_VERSION) || (currentVersion === null);
-
-  // Perform the migration
-  if ( needMigration && game.user.isGM ) {
-    migrations.migrateWorld();
-  }
 });
 
 /*
@@ -355,10 +345,11 @@ Hooks.once("ready", function() {
 
 // getSceneControlButtons
 Hooks.on("renderSceneControls", async (app, html) => {
-  let dice_roller = $('<li class="scene-control" data-tooltip="Dice Roll"><i class="fas fa-dice"></i></li>');
-  dice_roller.click( async function() {
+  let dice_roller = $(
+    '<li class="scene-control" data-tooltip="Dice Roll"><i class="fas fa-dice"></i></li>'
+  );
+  dice_roller.click(async function () {
     await simpleRollPopup();
   });
-  html.children().first().append( dice_roller );
-
+  html.children().first().append(dice_roller);
 });
