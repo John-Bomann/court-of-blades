94c7ebc4e65c30ce486975b927b0fa319d4cb7c6
John Bomann <johnabomann@gmail.com>
2024-07-27 15:16:26 -0500

    Base commit

---
+++ module/blades-actor-sheet.js
@@ -0,0 +1,279 @@
+import { BladesSheet } from "./blades-sheet.js";
+import { BladesActiveEffect } from "./blades-active-effect.js";
+import { BladesHelpers } from "./blades-helpers.js";
+
+/**
+ * Extend the basic ActorSheet with some very simple modifications
+ * @extends {BladesSheet}
+ */
+export class BladesActorSheet extends BladesSheet {
+  /** @override */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["blades-in-the-dark", "sheet", "actor", "pc"],
+      template: "systems/blades-in-the-dark/templates/actor-sheet.html",
+      width: 790,
+      height: 890,
+      tabs: [{ navSelector: ".tabs", contentSelector: ".tab-content", initial: "abilities" }],
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  async getData(options) {
+    const superData = super.getData(options);
+    const sheetData = superData.data;
+    sheetData.owner = superData.owner;
+    sheetData.editable = superData.editable;
+    sheetData.isGM = game.user.isGM;
+
+    // Prepare active effects
+    sheetData.effects = BladesActiveEffect.prepareActiveEffectCategories(this.actor.effects);
+
+    // Calculate Load
+    let loadout = 0;
+    sheetData.items.forEach((i) => {
+      loadout += i.type === "item" ? parseInt(i.system.load) : 0;
+    });
+
+    //Sanity Check
+    if (loadout < 0) {
+      loadout = 0;
+    }
+    if (loadout > 10) {
+      loadout = 10;
+    }
+
+    sheetData.system.loadout = loadout;
+
+    // Encumbrance Levels
+    let load_level = [
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Normal",
+      "BITD.Normal",
+      "BITD.Heavy",
+      "BITD.Encumbered",
+      "BITD.Encumbered",
+      "BITD.Encumbered",
+      "BITD.OverMax",
+    ];
+    let mule_level = [
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Light",
+      "BITD.Normal",
+      "BITD.Normal",
+      "BITD.Heavy",
+      "BITD.Encumbered",
+      "BITD.OverMax",
+    ];
+    let mule_present = 0;
+
+    //look for Mule ability
+    // @todo - fix translation.
+    sheetData.items.forEach((i) => {
+      if (i.type === "ability" && i.name === "(C) Mule") {
+        mule_present = 1;
+      }
+    });
+
+    //set encumbrance level
+    if (mule_present) {
+      sheetData.system.load_level = mule_level[loadout];
+    } else {
+      sheetData.system.load_level = load_level[loadout];
+    }
+
+    sheetData.system.load_levels = {
+      "BITD.Light": "BITD.Light",
+      "BITD.Normal": "BITD.Normal",
+      "BITD.Heavy": "BITD.Heavy",
+    };
+
+    sheetData.system.description = await TextEditor.enrichHTML(sheetData.system.description, {
+      secrets: sheetData.owner,
+      async: true,
+    });
+
+    // catch unmigrated actor data
+    for (const a in sheetData.system.attributes) {
+      for (const s in sheetData.system.attributes[a].skills) {
+        if (
+          sheetData.system.attributes[a].skills[s].max === undefined ||
+          sheetData.system.attributes[a].skills[s].max === 4
+        ) {
+          sheetData.system.attributes[a].skills[s].max = 3;
+        }
+
+        //include Active Effect alterations to skill minimums
+        if (
+          sheetData.system.attributes[a].skills[s].value <=
+          sheetData.system.attributes[a].skills[s].min
+        ) {
+          sheetData.system.attributes[a].skills[s].value =
+            sheetData.system.attributes[a].skills[s].min;
+        }
+      }
+    }
+
+    //check for additional stress, trauma, and mastery from crew
+    //check for a crew and get its data
+    if (sheetData.system.crew.length > 0) {
+      let crew_id = sheetData.system.crew[0].id;
+      let crew_actor = game.actors.get(crew_id);
+      //add to max stress
+      sheetData.system.stress.max =
+        sheetData.system.stress.max + crew_actor.system.scoundrel.add_stress;
+      //add to max trauma
+      sheetData.system.trauma.max =
+        sheetData.system.trauma.max + crew_actor.system.scoundrel.add_trauma;
+      //check for mastery
+      let has_mastery = crew_actor.system.scoundrel.mastery;
+      if (has_mastery) {
+        for (const b in sheetData.system.attributes) {
+          for (const t in sheetData.system.attributes[b].skills) {
+            if (sheetData.system.attributes[b].skills[t].max === 3) {
+              sheetData.system.attributes[b].skills[t].max = 4;
+            }
+          }
+        }
+      }
+    }
+
+    return sheetData;
+  }
+
+  /** @override **/
+  async _onDropItem(event, droppedItem) {
+    await super._onDropItem(event, droppedItem);
+    if (!this.actor.isOwner) {
+      ui.notifications.error(
+        `You do not have sufficient permissions to edit this character. Please speak to your GM if you feel you have reached this message in error.`,
+        { permanent: true }
+      );
+      return false;
+    }
+    await this.handleDrop(event, droppedItem);
+  }
+
+  /** @override **/
+  async _onDropActor(event, droppedActor) {
+    await super._onDropActor(event, droppedActor);
+    if (!this.actor.isOwner) {
+      ui.notifications.error(
+        `You do not have sufficient permissions to edit this character. Please speak to your GM if you feel you have reached this message in error.`,
+        { permanent: true }
+      );
+      return false;
+    }
+    await this.handleDrop(event, droppedActor);
+  }
+
+  /** @override **/
+  async handleDrop(event, droppedEntity) {
+    let droppedEntityFull = await fromUuid(droppedEntity.uuid);
+    switch (droppedEntityFull.type) {
+      case "npc":
+        await BladesHelpers.addAcquaintance(this.actor, droppedEntityFull);
+        break;
+      case "crew":
+        await BladesHelpers.addCrew(this.actor, droppedEntityFull);
+        break;
+      case "item":
+        break;
+      case "ability":
+        break;
+      case "class":
+        break;
+      default:
+        break;
+    }
+  }
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Update Inventory Item
+    html.find(".item-body").click((ev) => {
+      const element = $(ev.currentTarget).parents(".item");
+      const item = this.actor.items.get(element.data("itemId"));
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find(".item-delete").click(async (ev) => {
+      const element = $(ev.currentTarget).parents(".item");
+      await this.actor.deleteEmbeddedDocuments("Item", [element.data("itemId")]);
+      element.slideUp(200, () => this.render(false));
+    });
+
+    // manage active effects
+    html
+      .find(".effect-control")
+      .click((ev) => BladesActiveEffect.onManageActiveEffect(ev, this.actor));
+
+    // acquaintance status toggle
+    html.find(".standing-toggle").click((ev) => {
+      let acquaintances = this.actor.system.acquaintances;
+      let acqId = ev.target.closest(".acquaintance").dataset.acquaintance;
+      let clickedAcqIdx = acquaintances.findIndex((item) => item.id == acqId);
+      let clickedAcq = acquaintances[clickedAcqIdx];
+      let oldStanding = clickedAcq.standing;
+      let newStanding;
+      switch (oldStanding) {
+        case "friend":
+          newStanding = "rival";
+          break;
+        case "rival":
+          newStanding = "neutral";
+          break;
+        case "neutral":
+          newStanding = "friend";
+          break;
+      }
+      clickedAcq.standing = newStanding;
+      acquaintances.splice(clickedAcqIdx, 1, clickedAcq);
+      this.actor.update({ system: { acquaintances: acquaintances } });
+    });
+
+    // Open Acquaintance
+    html.find(".open-friend").click((ev) => {
+      const element = $(ev.currentTarget).parents(".item");
+      const actor = game.actors.get(element.data("itemId"));
+      actor?.sheet.render(true);
+    });
+
+    // Remove Acquaintance from character sheet
+    html.find(".acquaintance-delete").click((ev) => {
+      //let acqId = ev.target.closest('.acquaintance').dataset.acquaintance; //used when <div class="acquaintance"
+      const element = $(ev.currentTarget).parents(".item");
+      let acqId = element.data("itemId");
+      BladesHelpers.removeAcquaintance(this.actor, acqId);
+    });
+
+    // Import Acquaintance by playbook
+    html.find(".import-contacts").click((ev) => {
+      const playbook = this.actor.items.filter((i) => i.type === "class")[0]?.name;
+      BladesHelpers.import_pb_contacts(this.actor, playbook);
+    });
+
+    // Remove Crew from character sheet
+    html.find(".crew-delete").click((ev) => {
+      const element = $(ev.currentTarget).parents(".item");
+      let crewId = element.data("itemId");
+      BladesHelpers.removeCrew(this.actor, crewId);
+    });
+  }
+}
